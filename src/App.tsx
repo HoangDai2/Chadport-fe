ced22dca5c365efb5a7596f95b23bf455e16ef7b
Duy Phong <easylmvp@gmail.com>
2024-10-03 17:46:39 +0700

    creat login

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import "./style/responsive.css";
 import "./style/toast.css";
 import "./style/Home.css"
 import "bootstrap/dist/css/bootstrap.min.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
 import HeaderClient from "./components/HeaderClient";
 import FooterClient from "./components/FooterClient";
 import Banner from "./components/Banner";
@@ -14,6 +13,16 @@ import ProductSale from "./components/ProductSale";
 import Admin from "./admin/Admin";
 import ListUser from "./admin/pages/ListUser";
 import Home from "./pages/Home";
+import ShopList from "./pages/ShopList";
+import "./libs/feather-font/css/iconfont.css";
+import './libs/icomoon-font/css/icomoon.css';
+// import './libs/font-awesome/css/font-awesome.css';
+import './libs/wpbingofont/css/wpbingofont.css';
+import './libs/elegant-icons/css/elegant.css';
+import './libs/slick/css/slick.css';
+import './libs/slick/css/slick-theme.css';
+import './libs/mmenu/css/mmenu.min.css';
+import LoginRegister from "./pages/Login";
 function App() {
   return (
     <>
@@ -33,7 +42,30 @@ function App() {
             </>
           }
         />
-
+        <Route
+          path="/shoplist"
+          element={
+            <>
+              <HeaderClient />
+              <div className="content" style={{ padding: "70px" }}>
+                <ShopList />
+              </div>
+              <FooterClient />
+            </>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <>
+              <HeaderClient />
+              <div className="content" style={{ padding: "70px" }}>
+                <LoginRegister />
+              </div>
+              <FooterClient />
+            </>
+          }
+        />
         {/* Router admin */}
         <Route path="/admin" element={<Admin />}>
           <Route index element={<div>Welcome to Admin Dashboard</div>} />
